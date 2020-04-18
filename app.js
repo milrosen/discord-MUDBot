0a3b7b3b23d7a6de0a771b5d05d5169eb35f94b4
milrosen <61394098+milrosen@users.noreply.github.com>
2020-04-18 13:32:33 -0600

    added dotenv so I could test locally

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const {prefix} = require('./config')
 
+
+// keep BOT_TOKEN in its own file and also on Heroku
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  console.log('not in production')
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });

046a414336d932ff2a555470c9027b1456c7c3ae
milrosen <61394098+milrosen@users.noreply.github.com>
2020-05-03 19:01:14 -0600

    added dynamic cooldown commmand to command handler and Sequelize support

--- app.js
+++ app.js
@@ -2,13 +2,7 @@
 const Discord = require('discord.js');
 const clientDs = new Discord.Client();
 clientDs.commands = new Discord.Collection();
-const PostgreSQL = require('pg');
-const clientPg = new PostgreSQL.Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: true,
-});
 const fs = require('fs');
-
 // require/setup stuff I've written
 const { prefix } = require('./config');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -19,10 +13,13 @@ if (process.env.NODE_ENV !== 'production') {
 	console.log('not in production');
 }
 
-// initialize heroku PostgreSQL
-clientPg.connect();
+// initialize Sequelize
+const { Sequelize, Op } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 // discord section
+const cooldowns = new Discord.Collection();
+
 clientDs.once('ready', () => {
 	console.log('Ready!');
 });
@@ -38,13 +35,32 @@ for (const file of commandFiles) {
 
 clientDs.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
 	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-	if (!clientDs.commands.has(command)) return;
+	if (!clientDs.commands.has(commandName)) return;
+	const command = clientDs.commands.get(commandName);
 
+	// cooldowns
+	if (!cooldowns.has(command.name)) cooldowns.set(command.name, new Discord.Collection());
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply((typeof (command['cooldownMessage']) === 'function') ? command.cooldownMessage(timeLeft) : `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	// execute command
 	try {
-		clientDs.commands.get(command).execute(message, args);
+		command.execute(message, args, sequelize, Op);
 	}
 	catch (error) {
 		console.error(error);

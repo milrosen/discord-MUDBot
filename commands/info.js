b43ea858d09307065c651374b4fd0481ede5501f
milrosen <61394098+milrosen@users.noreply.github.com>
2020-05-06 16:17:18 -0600

    added the info command default behavior

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'calls the info method in other commands',
 	execute: async ({ msg, arg }, ...rest) => {
 		const commands = rest[0];
-		if (arg) {
+		if (arg[0]) {
 			if (commands.has(arg[0])) {
 				const command = commands.get(arg[0]);
 				if (Object.hasOwnProperty.call(command, 'info')) command.info({ 'msg': msg, 'arg': arg });
@@ -17,6 +17,13 @@ module.exports = {
 					msg.channel.send('there was an error with this info command, try using info with no arguments instead');
 				}
 			}
+		}
+		else {
+			let str = 'Commands:\n';
+			commands.forEach(cmd => {
+				str += `	${cmd.name}: ${cmd.description}\n`;
+			});
+			msg.channel.send(str);
 		}
 	},
 	defaultInfo: async ({ msg }) => {
